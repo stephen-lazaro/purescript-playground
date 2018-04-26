96f52af75e4684104803561cb2b407dd893ed127
Stephen Lazaro <stephen@earnest.com>
2018-04-25 22:50:21 -0700

    Grr. no type synonym instances
    tho that's fair

--- src/Main.purs
+++ src/Main.purs
@@ -4,7 +4,8 @@ import Prelude
 import Control.Monad.Eff (Eff)
 import Control.Monad.Eff.Console (CONSOLE, log)
 import Data.Foldable (foldl)
-import Data.List (List(..))
+import Data.List (List(..), head)
+import Data.Maybe (Maybe)
 import Math (pi)
 
 circleArea :: Number -> Number
@@ -24,6 +25,18 @@ type Address =
   , city :: String
   , state :: String
   }
+
+--instance eqAddr :: Eq Address where
+--  eq addressA addressB =
+--    addressA.street == addressB.street &&
+--      addressA.city == addressB.city &&
+--      addressA.state == addressB.state
+
+--instance eqEntry :: Eq Entry where
+--  eq recordA recordB =
+--    recordA.firstName == recordB.firstName &&
+--      recordA.lastName == recordB.lastName &&
+--      recordA.address == recordB.address
 
 type AddressBook = List Entry
 
@@ -48,7 +61,22 @@ filter f (Cons hd rst) = case (f hd) of
   true -> Cons hd (filter f rst)
   false -> filter f rst
 
+findEntry :: String -> String -> AddressBook -> Maybe Entry
+findEntry firstName lastName = head <<< filter filterEntry
+  where
+    filterEntry :: Entry -> Boolean
+    filterEntry entry =
+      entry.firstName == firstName &&
+        entry.lastName == lastName
 
+findByAddress :: Address -> AddressBook -> Maybe Entry
+findByAddress address = head <<< filter findAddress
+  where
+    findAddress :: Entry -> Boolean
+    findAddress entry =
+      entry.address.street == address.street &&
+        entry.address.city == address.city &&
+        entry.address.state == address.state
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
