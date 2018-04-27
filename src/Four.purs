42698534e88bb1cae78505a724c6a5f5dc5d51ea
Stephen Lazaro <stephen@earnest.com>
2018-04-26 21:44:35 -0700

    Some fixings, some more functions

--- src/Four.purs
+++ src/Four.purs
@@ -16,6 +16,16 @@ isEvenLocal x = isEvenLocalAcc 2
 allEvenLocal :: List Int -> Int
 allEvenLocal = (filter isEvenLocal) >>> length
 
+squaresOfStuff :: List Number -> List Number
+squaresOfStuff = map \x -> x * x
+
+filterNegatives :: List Number -> List Number
+filterNegatives = filter \x -> x >= 0.0
+
+infixl 0 filter as <$?>
+
+filterNegativesOther :: List Number -> List Number
+filterNegativesOther lz = (\x -> x >= 0.0) <$?> lz
 
 main :: forall e. Eff (console :: CONSOLE | e) Unit
 main = do
